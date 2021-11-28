b3aad4e2c5392c3bb0d23b712342752bc86d26e0
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-28 21:40:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+bool l_o(char &c) {
+    //DICHIARAZIONE VARIABILI
+    bool b = false;
+    if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        b = true;
+        if (c <= 'Z') { //Lettere maiuscole
+            c = c - 'A' + 'a'; //32;
+        }
+        else { //Viceversa
+            c = c - 'a'+'A';
+        }
+    }
+    return b;
+}

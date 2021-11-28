678e203dccccee74e71ce544b8b54f8b8c060919
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-28 21:42:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    //DICHIARAZIONE VARIABILI
+
+    char i = '0';
+
+    //INPUT VARIABILI
+
+    cin >> i;
+
+    //CHIHAMO FUNZIONE DA LIBRERIA E PASSO IL VALORE IN INPUT
+
+  
+
+    bool conversione = l_o(i);
+    if (conversione) {
+        cout << i << endl;
+    }
+    else {
+        cout << "errore" << endl;
+    }
+
+
+    return 0;
 }
+

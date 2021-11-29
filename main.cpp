6ba9784261aa8a331d6f4a9b231bfed87467de91
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-29 19:47:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,2 @@
-#include <iostream>
-#include "lib.h"
-#include <string>
-using namespace std;
 
-int main() {
-    //DICHIARAZIONE VARIABILI
-    //B è IL VERO O FALSO RICHIESTO, D IL CARATTERE INSERITO TRASFORMATO, I L'INPUT
-
-    char i = '0';
-
-    //INPUT VARIABILI
-
-    cin >> i;
-
-    string conversione = l_o(i);
-    if (conversione.compare("Vero") == 0) {
-        cout << i << endl;
-    }
-    else {
-        cout << "errore" << endl;
-    }
-
-
-    return 0;
-}
 

30d9d99174b47b959d3906724b8c37b8cf6a2f1b
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-29 19:45:02 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,10 +1,1 @@
 
-
-#ifndef ESERITAZIONE_VALUTATA_6_1_LIB_H
-#define ESERITAZIONE_VALUTATA_6_1_LIB_H
-#include <string>
-using namespace std;
-
-string l_o(char &a);
-
-#endif //ESERITAZIONE_VALUTATA_6_1_LIB_H

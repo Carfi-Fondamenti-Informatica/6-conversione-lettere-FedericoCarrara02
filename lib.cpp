904e386a5d3c6cbf8faf3728f1d57d136e335124
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-29 19:54:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,15 +5,17 @@ using namespace std;
 string l_o(char &c) {
     //DICHIARAZIONE VARIABILI
     string b = "False";
-    if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+    
+    if (c >= 'A' && c <= 'Z') {
         b = "Vero";
-        if (c <= 'Z') { //Lettere maiuscole
-            c = c - 'A' + 'a'; //32;
-        }
-        else { //Viceversa
-            c = c - 'a'+'A';
-        }
+        c = c - 'A' + 'a';
+
     }
+    else if (c >= 'a' && c <= 'z') {
+        b = "Vero";
+        c = c - 'a'+'A';
+    }
+    
     return b;
 }
 
